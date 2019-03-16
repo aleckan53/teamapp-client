4b22c94edbb285c8016f7814bb89a3ccc7974a68
admin <admin@admins-MacBook-Pro-2.local>
2019-03-16 02:24:59 -0700

    add infinite scroll to Search component

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -3,28 +3,32 @@ import config from '../config'
 import './Search.css'
 
 import { ProjectCard } from '../Project/ProjectCard'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export const Search = props => {
   const [data, setData] = useState({
     projects: [],
-    users: [],
-    currentLength: 0,
-    totalLength: 0,
+    totalCount: 0,
     currentPage: 1,
-    totalPages: 1
+    totalPages: 1,
   })
 
   useEffect(()=>{
     fetch(`${config.API_ENDPOINT}/search/featured?page=${data.currentPage}`)
       .then(res=> res.json())
-      .then(res=> setData({
-        ...data,
-        projects: res.projects, 
-        totalLength: Number(res.count),
-        currentLength: res.projects.length,
-        totalPages: Math.ceil(res.count/5)
-      }))
+      .then(res=> {
+        setData({
+          projects: [...data.projects, ...res.projects],
+          totalCount: Number(res.count),
+          currentPage: data.currentPage,
+          totalPages: Math.ceil(res.count/7),
+        })
+      })
   }, [data.currentPage])
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
 
   const setNextPage = () => {
     return data.currentPage === data.totalPages
@@ -32,17 +36,8 @@ export const Search = props => {
       : setData({...data, currentPage: data.currentPage+1})
   }
 
-  const setPrevPage = () => {
-    return data.currentPage <= 1
-      ? data.currentPage
-      : setData({...data, currentPage: data.currentPage-1})
-  }
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    
-  }
   console.log(data)
+
   return <div className="Search">
     <header>
       <h2>Search</h2>
@@ -57,14 +52,20 @@ export const Search = props => {
         </fieldset>
       </form>
     </div>
-    <div className="Search__pagination">
-      <button onClick={()=>setPrevPage()}>Prev</button>
-      {/* page numbers go here */}
-      <button onClick={()=>setNextPage()}>Next</button>
+    <div id="scrolableInSearch">
+      <InfiniteScroll
+        scrollableTarget="scrolableInSearch"
+        dataLength={data.projects.length}
+        next={()=>setNextPage()}
+        hasMore={data.projects.length<data.totalCount}
+        loader={<h4>Loading...</h4>}
+        endMessage={<p className="bot-msg">That's it! You've reached the bottom.</p>}
+        >
+        {data.projects.map((project, i)=>{
+          return <ProjectCard key={i} {...project}/>
+        })}
+      </InfiniteScroll>
     </div>
-    {data.projects.map((project, i)=>{
-      return <ProjectCard key={i} {...project}/>
-    })}
   </div>
 }
 
